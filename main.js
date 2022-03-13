a32e0611a83517b7bf4146e8c629bce3cc2d534d
Denise Tham <denisetham@edelo.com.sg>
2022-03-13 18:52:01 +0800

    refactoring code

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var time = new Date();
     hour = time.getHours();
     minute = time.getMinutes();
@@ -6,27 +6,28 @@ $(document).ready(function() {
     var str = `[${hour}:${minute}:${second}]`;
     $('.time').html(str);
     console.log("Nice try. You won't find anything here.");
-    $(window).bind('click keypress', function() {
+    $(window).on('click keypress tap touchstart', function () {
         click++;
         if (click == 1) {
             $('.t1').addClass('active');
             speak('Hello Nicole');
-            setTimeout( function() {
+            setTimeout(function () {
                 $('.p2').show();
             }, 5000);
         }
         if (click == 2) {
             $('.t2').addClass('active');
             speak('Happy Birthday idiot');
-            setTimeout( function() {
+            setTimeout(function () {
                 window.location = "https://youtu.be/mhJhQddBEBU?t=3";
             }, 5000);
         }
     })
-    
+
 });
 
 var click = 0;
+
 function speak(str) {
     var msg = new SpeechSynthesisUtterance(str);
     msg.rate = 0.5;
