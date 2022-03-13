e896efabecc1d8c7f19549be375694f844d95c29
Denise Tham <denisetham@edelo.com.sg>
2022-03-13 16:04:32 +0800

    initial commit.

---
+++ main.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    var time = new Date();
+    hour = time.getHours();
+    minute = time.getMinutes();
+    second = time.getSeconds();
+    var str = `[${hour}:${minute}:${second}]`;
+    $('.time').html(str);
+    console.log("Nice try. You won't find anything here.");
+    $(window).bind('click keypress', function() {
+        click++;
+        if (click == 1) {
+            $('.t1').addClass('active');
+            speak('Hello Nicole');
+            setTimeout( function() {
+                $('.p2').show();
+            }, 5000);
+        }
+        if (click == 2) {
+            $('.t2').addClass('active');
+            speak('Happy Birthday idiot');
+            setTimeout( function() {
+                window.location = "https://youtu.be/mhJhQddBEBU?t=3";
+            }, 5000);
+        }
+    })
+    
+});
+
+var click = 0;
+function speak(str) {
+    var msg = new SpeechSynthesisUtterance(str);
+    msg.rate = 0.5;
+    msg.pitch = 0.5;
+    window.speechSynthesis.speak(msg);
+}
